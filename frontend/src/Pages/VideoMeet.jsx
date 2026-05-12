e1bba6064b7107369334837d3a9a90d29c4ecf5e
jay finaviya <jayfinaviya5@gmail.com>
2026-05-12 22:39:14 +0530

    Add Project Files

--- frontend/src/Pages/VideoMeet.jsx
+++ frontend/src/Pages/VideoMeet.jsx
@@ -11,6 +11,39 @@ const peerConfigConnections = {
 }
 
 export default function VideoMeet() {
+
+     var socketRef = useRef();
+    let socketIdRef = useRef();
+
+    let localVideoref = useRef();
+
+    let [videoAvailable, setVideoAvailable] = useState(true);
+
+    let [audioAvailable, setAudioAvailable] = useState(true);
+
+    let [video, setVideo] = useState([]);
+
+    let [audio, setAudio] = useState();
+
+    let [screen, setScreen] = useState();
+
+    let [showModal, setModal] = useState(true);
+
+    let [screenAvailable, setScreenAvailable] = useState();
+
+    let [messages, setMessages] = useState([])
+
+    let [message, setMessage] = useState("");
+
+    let [newMessages, setNewMessages] = useState(3);
+
+    let [askForUsername, setAskForUsername] = useState(true);
+
+    let [username, setUsername] = useState("");
+
+    const videoRef = useRef([])
+
+    let [videos, setVideos] = useState([])
     
   return (
     <div>VideoMeet</div>
